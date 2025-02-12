96f16b84a998d5c61722f2cc13c672c553a7e53c
aziham <hamza.aziane@pm.me>
2025-02-12 19:54:30 +0100

    Add reset.css for consistent styling across browsers

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
+import './reset.css';
+
 import App from './components/App';
 
 const root = createRoot(document.querySelector('#root'));
